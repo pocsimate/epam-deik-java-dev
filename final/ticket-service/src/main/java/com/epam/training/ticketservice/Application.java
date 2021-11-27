f0836b998becd1ce350a7fa6e5a871a1a6693701
pocsimate <pocsimate@icloud.com>
2021-11-27 14:00:58 +0100

    Added spring boot support to project

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
@@ -1,5 +1,8 @@
 package com.epam.training.ticketservice;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
     }
